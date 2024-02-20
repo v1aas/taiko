d6d404f412c334a5e8815ddc9f29289a08f39684
Vlas <dron.2001sv@mail.ru>
2024-02-20 15:34:05 +0300

    add config

---
+++ config.py
@@ -0,0 +1,18 @@
+# RPC
+RPC = "https://rpc.katla.taiko.xyz"
+
+# Rate
+USDC_TO_ETH = 0.118
+HORSE_TO_ETH = 0.0001
+
+# Count transactions
+MIN = 2
+MAX = 5
+
+# Delay between transactions
+MIN_SEC_TXN = 10
+MAX_SEC_TXN = 20
+
+# Delay between wallets
+MIN_SEC_WALL = 30
+MAX_SEC_WALL = 120
